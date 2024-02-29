dcbd862379362b754dabe1496f2d8d35bb74d27d
cabin36 <cabin36@naver.com>
2024-02-29 09:40:23 +0900

    [전자결재-7]

--- src/main/java/com/example/vet/controller/work/eSign/eSignDraft_Controller.java
+++ src/main/java/com/example/vet/controller/work/eSign/eSignDraft_Controller.java
@@ -1,13 +1,17 @@
 package com.example.vet.controller.work.eSign;
 
+import com.example.vet.model.AnimalVO;
+import com.example.vet.model.MasterVO;
 import com.example.vet.model.Member;
 import com.example.vet.model.SignDocument;
+import com.example.vet.model.adopt.MissedAnimal;
 import com.example.vet.service.work.eSign.eSignDraft_Service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -41,6 +45,7 @@ public class eSignDraft_Controller {
 
     @GetMapping("draftInsert")
     public String draftInsert(SignDocument signDocument){
+
         return "pages/esignbox/docsBox";
     }
 
@@ -50,13 +55,60 @@ public class eSignDraft_Controller {
      기능 : 결재선 선택 기능
      **********************************************************************************/
 
-    @GetMapping("/selectLine") // 요청 URL을 정확하게 지정
     @ResponseBody
+    @GetMapping("selectLine") // 요청 URL을 정확하게 지정
     public List<Member> selectLine(Model model) { // 파라미터 명을 JSP 파일에서 보낸 데이터와 일치시킴
         log.info("결재선 조회");
         List<Member> lineList = eSignDraft_service.findLine();
         // model.addAttribute("lineList", lineList);
         log.info(lineList.toString());
-        return eSignDraft_service.findLine();
+        return lineList;
     }
+
+    /**********************************************************************************
+     작성자 : 최윤정
+     작성일자 : 26.02.25
+     기능 : 입양자 선택 기능
+     **********************************************************************************/
+    @ResponseBody
+    @GetMapping("adopterList")
+    public List<MasterVO> adopterList() {
+        List<MasterVO> abandonList =  eSignDraft_service.selectAdopter();
+        log.info("입양자 리스트");
+        log.info(abandonList.toString());
+        return abandonList;
+    }
+
+    /**********************************************************************************
+     작성자 : 최윤정
+     작성일자 : 26.02.25
+     기능 : 유기동물 선택 기능
+     **********************************************************************************/
+
+    @GetMapping("abandonList")
+    @ResponseBody
+    public List<MissedAnimal> abandonList() {
+        // MissedAnimal missedAnimal = null;
+        List<MissedAnimal> abandonList = eSignDraft_service.selectAbandon();
+        // model.addAttribute("abandonList", abandonList);
+        log.info("유기동물 리스트");
+        log.info(abandonList.toString());
+        return eSignDraft_service.selectAbandon();
+    }
+
+    /**********************************************************************************
+     작성자 : 최윤정
+     작성일자 : 26.02.25
+     기능 : 기안서 작성
+     **********************************************************************************/
+    // @GetMapping("adopterList")
+    // public String eSignInsert(Model model, SignDocument signDocument) {
+    //     MasterVO masterVO = null;
+    //     List<MasterVO> aList = eSignDraft_Service.selectAdopter();
+    //     model.addAttribute("aList", aList);
+    //     log.info(aList.toString());
+    //     return "pages/customerDB/TotalCustomerList";
+    // }
+
 }
+
--- src/main/java/com/example/vet/repository/work/eSign/eSignDraft_Repository.java
+++ src/main/java/com/example/vet/repository/work/eSign/eSignDraft_Repository.java
@@ -1,12 +1,15 @@
 package com.example.vet.repository.work.eSign;
 
+import com.example.vet.model.MasterVO;
 import com.example.vet.model.Member;
+import com.example.vet.model.adopt.MissedAnimal;
 import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Repository
@@ -17,9 +20,26 @@ public class eSignDraft_Repository {
     public eSignDraft_Repository(SqlSessionTemplate sqlSessionTemplate) {
         this.sqlSessionTemplate = sqlSessionTemplate;
     }
+
+    public List<MasterVO> selectAdopter() {
+        List<MasterVO> aList;
+        aList = sqlSessionTemplate.selectList("selectAdopter");
+        return aList;
+    }
+
     public List<Member> findLine() {
         List<Member> lineList = new ArrayList<>();
         lineList = sqlSessionTemplate.selectList("selectLine");
         return lineList;
+    }
+
+    public List<MissedAnimal> selectAbandon() {
+        List<MissedAnimal> abandonList = sqlSessionTemplate.selectList("selectAbandon");
+        // selectList 메서드가 null을 반환하는 경우 널 체크
+        if (abandonList == null) {
+            return Collections.emptyList();
+        }
+        return abandonList;
+
     }
 }
