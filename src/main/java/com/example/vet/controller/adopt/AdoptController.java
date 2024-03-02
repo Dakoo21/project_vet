68e0038b7e1ccb49463eaa15cb75fc75cb3edeb6
cabin36 <cabin36@naver.com>
2024-03-02 22:11:11 +0900

    [test]

--- src/main/java/com/example/vet/controller/adopt/AdoptController.java
+++ src/main/java/com/example/vet/controller/adopt/AdoptController.java
@@ -1,25 +1,25 @@
-package com.example.vet.controller.adopt;
-
-
-import com.example.vet.model.adopt.MissingAnimalResVO;
-import com.example.vet.service.adopt.AdoptService;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@Slf4j
-@RequestMapping("/api/v1/adopt")
-@RequiredArgsConstructor
-public class AdoptController {
-
-    private AdoptService adoptService;
-
-    @GetMapping("/missed")
-    public MissingAnimalResVO getMissedAnimal(){
-        return new MissingAnimalResVO();
-    }
-
-}
+//package com.example.vet.controller.adopt;
+//
+//
+//import com.example.vet.model.adopt.MissingAnimalResVO;
+//import com.example.vet.service.adopt.AdoptService;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@Slf4j
+//@RequestMapping("/api/v1/adopt")
+//@RequiredArgsConstructor
+//public class AdoptController {
+//
+//    private AdoptService adoptService;
+//
+//    @GetMapping("/missed")
+//    public MissingAnimalResVO getMissedAnimal(){
+//        return new MissingAnimalResVO();
+//    }
+//
+//}
--- src/main/java/com/example/vet/service/adopt/AdoptService.java
+++ src/main/java/com/example/vet/service/adopt/AdoptService.java
@@ -1,44 +1,44 @@
-package com.example.vet.service.adopt;
-
-
-import com.example.vet.config.rest_template.RestTemplateConfig;
-import com.example.vet.model.adopt.MissedAnimalReqVO;
-import com.example.vet.model.adopt.MissingAnimalResVO;
-import com.fasterxml.jackson.dataformat.xml.XmlMapper;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-import org.springframework.web.util.UriComponents;
-import org.springframework.web.util.UriComponentsBuilder;
-
-@Service
-@RequiredArgsConstructor
-public class AdoptService {
-
-    private final RestTemplateConfig restTemplateConfig;
-
-    public MissingAnimalResVO getMissedAnimal(MissedAnimalReqVO reqVO){
-        String url = "http://apis.data.go.kr/1543061/abandonmentPublicSrvc/abandonmentPublic";
-        String key = "lEeq8XMbXQj%2B2g%2BUio5fX1q0NYDwSfIAzHJNv3Uf9pABbQsTJZPpNda184SYmT8bxUhvlf4zoyXN%2F6BavDz3jQ%3D%3D";
-
-        MissedAnimalReqVO missedAnimalReqVO = new MissedAnimalReqVO(key,"20211201","20211231","1","10");
-
-        XmlMapper xmlMapper = new XmlMapper();
-
-        UriComponents uriComponents  = UriComponentsBuilder
-                .fromPath(url)
-                .queryParam("bgnde", missedAnimalReqVO.getBgnde())
-                .queryParam("endde", missedAnimalReqVO.getEndde())
-                .queryParam("pageNo",missedAnimalReqVO.getPageNo())
-                .queryParam("numOfRows",missedAnimalReqVO.getNumOfRows())
-                .queryParam("serviceKey",missedAnimalReqVO.getServiceKey())
-                .build();
-
-            String str   = restTemplateConfig.restTemplate()
-                .getForObject(uriComponents.toUriString(), String.class);
-
-            System.out.println(str);
-        return new MissingAnimalResVO();
-    }
-
-}
+//package com.example.vet.service.adopt;
+//
+//
+//import com.example.vet.config.rest_template.RestTemplateConfig;
+//import com.example.vet.model.adopt.MissedAnimalReqVO;
+//import com.example.vet.model.adopt.MissingAnimalResVO;
+//import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.stereotype.Service;
+//import org.springframework.web.util.UriComponents;
+//import org.springframework.web.util.UriComponentsBuilder;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class AdoptService {
+//
+//    private final RestTemplateConfig restTemplateConfig;
+//
+//    public MissingAnimalResVO getMissedAnimal(MissedAnimalReqVO reqVO){
+//        String url = "http://apis.data.go.kr/1543061/abandonmentPublicSrvc/abandonmentPublic";
+//        String key = "lEeq8XMbXQj%2B2g%2BUio5fX1q0NYDwSfIAzHJNv3Uf9pABbQsTJZPpNda184SYmT8bxUhvlf4zoyXN%2F6BavDz3jQ%3D%3D";
+//
+//        MissedAnimalReqVO missedAnimalReqVO = new MissedAnimalReqVO(key,"20211201","20211231","1","10");
+//
+//        XmlMapper xmlMapper = new XmlMapper();
+//
+//        UriComponents uriComponents  = UriComponentsBuilder
+//                .fromPath(url)
+//                .queryParam("bgnde", missedAnimalReqVO.getBgnde())
+//                .queryParam("endde", missedAnimalReqVO.getEndde())
+//                .queryParam("pageNo",missedAnimalReqVO.getPageNo())
+//                .queryParam("numOfRows",missedAnimalReqVO.getNumOfRows())
+//                .queryParam("serviceKey",missedAnimalReqVO.getServiceKey())
+//                .build();
+//
+//            String str   = restTemplateConfig.restTemplate()
+//                .getForObject(uriComponents.toUriString(), String.class);
+//
+//            System.out.println(str);
+//        return new MissingAnimalResVO();
+//    }
+//
+//}
